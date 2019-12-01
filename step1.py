9efc44eede57c0bfa7ff2b77d7117cdb0f23c6d9
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2019-12-01 16:01:14 -0600

    First working version

--- step1.py
+++ step1.py
@@ -4,24 +4,30 @@ sys.path.append(os.path.abspath(os.path.curdir))
 
 if __name__ == '__main__':
     energies = [1,3,5,10,15,20,25,30] # List of energies of generated particles
-    etas = [1.7]                      # List of etas to shoot particles
+    etaTags = ['1p7']                 # List of etas to shoot particles
+    etas = {}
+    etas['1p7'] = 1.7
     particles = [130]                 # List of particles to generate in pdg codes
     geometry = 'D41'                  # Geometry tag. Use >=D41
-    cmssw = os.environ('CMSSW_VERSION')
-    genDir = '$CMSSW_BASE/src/Configuration/GenProduction/python/'
+    cmssw = os.environ['CMSSW_VERSION']
+    cmsswBase = os.environ['CMSSW_BASE']
+    genDir = '%s/src/Configuration/GenProduction/python/'%cmsswBase
+    unitsPerJob = 50
+    njobs = 50
+    cwd = os.getcwd()
 
     for p in particles:
-        outTag='SingleK0L'
         for E in energies:
-            outTag='%s_%d'%(outTag,E)
-            for eta in etas:
-                outTag='%s%f'%eta
-                os.system('mkdir -p %s'%outTag)
-                os.system('cd %s'%outTag)
-                print('Creating configuration for K0L at E=%d Eta=%f.'%(E,eta))
+            for etaTag in etaTags:
+		outTag = 'SingleK0L'
+		outTag = '%s_E%d'%(outTag,E)
+                outTag = '%sEta%s'%(outTag,etaTag)
+		os.chdir(cwd)
+                os.system('mkdir -p myGeneration/%s'%outTag)
+                print('Creating configuration for K0L at E=%d Eta=%s.'%(E,etaTag))
 
                 # Create generator configurations
-                file0 = open('%s%s_pythia8_cfi.py'%(genDir,outTag))
+                file0 = open('%s%s_pythia8_cfi.py'%(genDir,outTag),'w')
                 file0.write("import FWCore.ParameterSet.Config as cms\n\n")
                 file0.write("generator = cms.EDFilter('Pythia8EGun',\n")
                 file0.write("\t\t\tPGunParameters = cms.PSet(\n")
@@ -29,9 +35,9 @@ if __name__ == '__main__':
                 file0.write("\tMinE = cms.double(%f),\n"%(E-0.01))
                 file0.write("\tParticleID = cms.vint32(%d),\n"%p)
                 file0.write("\tAddAntiParticle = cms.bool(True),\n")
-                file0.write("\tMaxEta = cms.double(%f),\n"%eta)
+                file0.write("\tMaxEta = cms.double(%f),\n"%etas[etaTag])
                 file0.write("\tMaxPhi = cms.double(3.14159265359),\n")
-                file0.write("\tMinEta = cms.double(%f),\n"%eta)
+                file0.write("\tMinEta = cms.double(%f),\n"%etas[etaTag])
                 file0.write("\tMinPhi = cms.double(-3.14159265359) ## in radians\n")
                 file0.write("\t),\n")
                 file0.write("\t\t\tVerbosity = cms.untracked.int32(0), ")
@@ -43,10 +49,11 @@ if __name__ == '__main__':
                 file0.close()
 
                 # Run cmsdriver.py to create workflows
-                os.system('cmsDriver.py Configuration/GenProduction/python/%s_pythia8_cfi.py \
-                --conditions auto:phase2_realistic -n 100 --era Phase2C8_timing_layer_bar \
-                --eventcontent FEVTDEBUG --relval 9000,50 -s GEN,SIM --datatier GEN-SIM \
-                --beamspot HLLHC --geometry Extended2023D41 --fileout file:step1.root')%outTag
+		os.chdir('%s/myGeneration/%s'%(cwd,outTag))
+		os.system('cmsDriver.py Configuration/GenProduction/python/%s_pythia8_cfi.py '
+		'--conditions auto:phase2_realistic -n 100 --era Phase2C8_timing_layer_bar '
+                '--eventcontent FEVTDEBUG --relval 9000,50 -s GEN,SIM --datatier GEN-SIM --no_exec '
+                '--beamspot HLLHC --geometry Extended2023D41 --fileout file:step1.root'%outTag)
 
                 # Create CRAB configuration file
                 file1 = open('crabConfig_%s.py'%outTag,'w')
@@ -55,7 +62,7 @@ if __name__ == '__main__':
                 file1.write('import config, getUsernameFromSiteDB\n')
                 file1.write('config = config()\n')
                 file1.write("config.General.requestName = ")
-                file1.write("'%s_%s_upgrade2023_%s_step1'\n"%(outTag,cmssw,geometr))
+                file1.write("'%s_%s_upgrade2023_%s_step1'\n"%(outTag,cmssw,geometry))
                 file1.write("config.General.workArea = 'crab_projects'\n")
                 file1.write("config.General.transferOutputs = True\n")
                 file1.write("config.General.transferLogs = False\n\n")
@@ -75,3 +82,5 @@ if __name__ == '__main__':
                 file1.write("'%s_%s_upgrade2023_%s_step1'\n\n"%(outTag,cmssw,geometry))
                 file1.write("config.Site.storageSite = 'T3_US_FNALLPC'\n")
                 file1.close()
+
+		os.system('crab submit -c crabConfig_%s.py'%outTag)
