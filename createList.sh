5651d5b40f33a0d7abec064ca6d67a3603e31c72
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2020-03-06 23:18:15 -0600

    Fixed fetching data stage geometry tag bug

--- createList.sh
+++ createList.sh
@@ -6,6 +6,6 @@
 #         $3: particles
 
 echo "Fetching data from previous step. Hold tight!"
-python checkStatus.py -s $1 -E $2 -p $3 | grep "Output dataset:" | awk '{print substr($NF,0,length($NF))}' > myGeneration/list.txt
+python checkStatus.py -s $1 -E $2 -p $3 -g $4 | grep "Output dataset:" | awk '{print substr($NF,0,length($NF))}' > myGeneration/list.txt
 echo "Data fetched successfully!"
 sleep 2
