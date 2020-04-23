379b9b9494313062847373441c18cbda0b83d050
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2020-04-23 14:08:12 -0500

    Add phi loop to checkStatus.py

--- checkStatus.py
+++ checkStatus.py
@@ -15,9 +15,25 @@ if __name__ == '__main__':
         energies = [1,3,5,10,15,20,25,30]
 
     # List of etas to shoot particles
-    etaTags = ['1p7']
+    etaTags = options.eta
+    if etaTags is None or len(etaTags) == 0:
+        print(col.magenta+'Warning:'+col.endc+'Etas not specified. '
+        'Using default values that might not work in your case.')
+        etaTags = ['1p7']
     etas = {}
-    etas['1p7'] = 1.7
+    for etaTag in etaTags:
+        etas[etaTag] = float(etaTag.replace("p","."))
+
+    # List of phi to shoot particles
+    phiTags = options.phi
+    if phiTags is None or len(phiTags) == 0:
+        print(col.magenta+'Warning:'+col.endc+'Phi not specified. '
+        'The script is not going to specify a Phi.')
+        phiTags = ['notSet']
+    phis = {}
+    for phiTag in phiTags:
+        if phiTag != 'notSet':
+            phis[phiTag] = float(phiTag.replace("p","."))
 
     # List of particles to generate in pdg codes
     particles = options.particles
@@ -35,22 +51,29 @@ if __name__ == '__main__':
     for p in particles:
         for E in energies:
             for etaTag in etaTags:
-                particleTag = particleTags[p]
-                outTag = 'Single%s'%particleTag
-                outTag = '%s_E%d'%(outTag,E)
-                outTag = '%sEta%s'%(outTag,etaTag)
-                print('Checking status for %s at E=%d Eta=%s.'%(particleTag,E,etaTag))
-                os.chdir(cwd)
-                os.chdir('myGeneration/%s/crab_projects/'%outTag)
-                if options.tag is None or options.tag == None:
-                    os.system('ls | grep %s | grep %s '
-                    '> submissions.txt'%(options.step,options.geometry))
-                else:
-                    os.system('ls | grep %s | grep %s | grep %s '
-                    '> submissions.txt'%(options.step,options.geometry,options.tag))
-                fSubmissions = open('submissions.txt','r')
-                for submission in fSubmissions:
-                    os.system('crab status -d %s > log.txt'%(submission))
-                    os.system('tail -n +9 log.txt | head -n -8')
-                    os.system('rm log.txt')
-                os.system('rm submissions.txt')
+                for phiTag in phiTags:
+                    particleTag = particleTags[p]
+                    outTag = 'Single%s'%particleTag
+                    outTag = '%s_E%d'%(outTag,E)
+                    outTag = '%sEta%s'%(outTag,etaTag)
+                    if phiTag != 'notSet':
+                        outTag = '%sPhi%s'%(outTag,phiTag)
+
+                    if phiTag != 'notSet':
+                        print('Checking status for %s at E=%d Eta=%s.'%(particleTag,E,etaTag))
+                    else:
+                        print('Checking status for %s at E=%d Eta=%s Phi=%s.'%(particleTag,E,etaTag,phiTag))
+                    os.chdir(cwd)
+                    os.chdir('myGeneration/%s/crab_projects/'%outTag)
+                    if options.tag is None or options.tag == None:
+                        os.system('ls | grep %s | grep %s '
+                        '> submissions.txt'%(options.step,options.geometry))
+                    else:
+                        os.system('ls | grep %s | grep %s | grep %s '
+                        '> submissions.txt'%(options.step,options.geometry,options.tag))
+                    fSubmissions = open('submissions.txt','r')
+                    for submission in fSubmissions:
+                        os.system('crab status -d %s > log.txt'%(submission))
+                        os.system('tail -n +9 log.txt | head -n -8')
+                        os.system('rm log.txt')
+                    os.system('rm submissions.txt')
