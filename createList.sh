416dddf9c657235130684890614846d978987d51
Christos Papageorgakis <christos.papageorgakis@cern.ch>
2020-04-25 17:42:21 -0500

    Add phi capabilities to step2 and createList

--- createList.sh
+++ createList.sh
@@ -4,10 +4,32 @@
 #         $1: step
 #         $2: energies
 #         $3: particles
+#         $4: geometry
+#         $5: eta
+#         $6: phi
 
-echo "Fetching data from previous step. Hold tight!"
-python checkStatus.py -s $1 -E $2 -p $3 -g $4 | grep "Output dataset:" | awk '{print substr($NF,0,length($NF))}' > myGeneration/list.txt
-echo "Data fetched successfully!"
+echo "\033[93mFetching\033[0m data from previous step. Hold tight!"
+
+if [ -z $5 ]
+then
+  if [ -z $6 ]
+  then
+    python checkStatus.py -s $1 -E $2 -p $3 -g $4 | grep "Output dataset:" | \
+    awk '{print substr($NF,0,length($NF))}' > myGeneration/list.txt
+  else
+    python checkStatus.py -s $1 -E $2 -p $3 -g $4 -P $6 | grep "Output dataset:" | \
+    awk '{print substr($NF,0,length($NF))}' > myGeneration/list.txt
+  fi
+elif [ -z $6]
+then
+  python checkStatus.py -s $1 -E $2 -p $3 -g $4 -e $5 | grep "Output dataset:" | \
+  awk '{print substr($NF,0,length($NF))}' > myGeneration/list.txt
+else
+  python checkStatus.py -s $1 -E $2 -p $3 -g $4 -e $5 -P $6 | grep "Output dataset:" | \
+  awk '{print substr($NF,0,length($NF))}' > myGeneration/list.txt
+fi
+
+echo "Data fetched \033[92msuccessfully\033[0m!"
 echo "Going to use the following datasets:"
 cat myGeneration/list.txt
 sleep 2
