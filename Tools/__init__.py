46843de947479496e4614ecdd02a778ae8e8fb18
Christos Papageorgakis <Christos.Papageorgakis@cern.ch>
2021-07-04 19:34:47 -0400

    Step 1 accepts numbers for input

--- Tools/__init__.py
+++ Tools/__init__.py
@@ -8,33 +8,68 @@
 import argparse
 
 def mainParser():
-    parser = argparse.ArgumentParser(description='Submit and manage multiple particle gun jobs with CRAB3.',usage='%(prog)s [options]')
-    parser.add_argument('-s','--step', help='Step to be used.',choices=['step1','step2','step3','ntuples'],required=True)
-    parser.add_argument('-g','--geometry', help='Detector geometry for tagging. (Default is D76)',default='D76')
-    parser.add_argument('-n','--njobs',type=int, help='Number of jobs to run. (Default is 10)',default='10')
-    parser.add_argument('-u','--unitsPerJob',type=int, help='Events per job for step1 and files processed per job for all other steps. (Default is 10)',default='10')
-    parser.add_argument('-E','--energies',type=int, help='List of energies to shoot.',nargs='*')
-    parser.add_argument('-e','--eta', help='List of eta to shoot.',nargs='*')
-    parser.add_argument('-P','--phi', help='List of phi to shoot.',nargs='*')
-    parser.add_argument('-p','--particles',type=int, help='List of particles to shoot.',nargs='*')
-    parser.add_argument('-t','--tag', help='Unique tag to discern between different submissions.',default=None)
-    parser.add_argument('-i','--inputTag', help='Tag of input dataset.',default=None)
-    parser.add_argument('-c','--campaign', help='Adds a tag to outputDatasetTag.',default=None)
-    parser.add_argument('-S','--site', help='Changes the output site. (Default is T3_US_FNALLPC)',default='T3_US_FNALLPC')
-    parser.add_argument('-d','--dest', help='Changes the output destination path. (Default is /store/user/)',default='/store/user/')
-    parser.add_argument('-a','--pileup', help='Generate samples with pileup. (Default is False)',default=False)
-    parser.add_argument('-C','--conditions', help='Conditions option passed to cmsDriver.py. (Default is phase2_realistic_T21)',default='phase2_realistic_T21')
-    parser.add_argument('-R','--era', help='Era option passed to cmsDriver.py. (Default is Phase2C11I13M9)',default='Phase2C11I13M9')
-    parser.add_argument('-m','--memory', help='Override max memory setting in MB for CRAB. (Default is set by CRAB)',default=None)
-    parser.add_argument('-N','--cpu', help='Override number of cores per job. (Default is 1)',default=None)
-    parser.add_argument('--no_exec', help='Prepare scripts but do not submit.',action='store_false')
-    parser.add_argument('--closeBy', help='Use CloseByParticleGunProducer instead of Pythia8EGun.',action='store_true')
-    parser.add_argument('--maxEn', help='Maximum of energy range in case of continuous energy distribution. (Default is 650 GeV)',default=None)
-    parser.add_argument('--minEn', help='Minimum of energy range in case of continuous energy distribution. (Default is 0 GeV)',default=None)
-    parser.add_argument('--maxEta', help='Maximum of eta range in case of continuous eta distribution. (Default is 3.0)',default=None)
-    parser.add_argument('--minEta', help='Minimum of eta range in case of continuous eta distribution. (Default is 1.5)',default=None)
-    parser.add_argument('--maxPhi', help='Maximum of phi range in case of continuous phi distribution. (Default is -pi)',default=None)
-    parser.add_argument('--minPhi', help='Minimum of phi range in case of continuous phi distribution. (Default is pi)',default=None)
+    parser = argparse.ArgumentParser(description='Submit and manage multiple particle gun jobs with CRAB3.',
+                                     usage='%(prog)s [options]')
+    parser.add_argument('-s', '--step', choices=['step1','step2','step3','ntuples'], required=True,
+                        help='Step to be used.')
+    parser.add_argument('-g', '--geometry', default='D76',
+                        help='Detector geometry for tagging. (Default is D76)')
+    parser.add_argument('-n', '--njobs', type=int, default='10',
+                        help='Number of jobs to run. (Default is 10)')
+    parser.add_argument('-u', '--unitsPerJob', type=int, default='10',
+                        help='Events per job for step1 and files processed per job for all other steps. '
+                             '(Default is 10)')
+    parser.add_argument('-E', '--energies', type=float, nargs='*',
+                        help='List of energies to shoot.')
+    parser.add_argument('-e', '--eta', type=float, nargs='*',
+                        help='List of eta to shoot.')
+    parser.add_argument('-P', '--phi', type=float, nargs='*',
+                        help='List of phi to shoot.')
+    parser.add_argument('-p', '--particles', type=int, nargs='*',
+                        help='List of particles to shoot.')
+    parser.add_argument('-t', '--tag',
+                        help='Unique tag to discern between different submissions.')
+    parser.add_argument('-i', '--inputTag',
+                        help='Tag of input dataset.')
+    parser.add_argument('-c', '--campaign',
+                        help='Adds a tag to outputDatasetTag.')
+    parser.add_argument('-S', '--site', default='T3_US_FNALLPC',
+                        help='Changes the output site. (Default is T3_US_FNALLPC)')
+    parser.add_argument('-d', '--dest', default='/store/user/',
+                        help='Changes the output destination path. (Default is /store/user/)')
+    parser.add_argument('-a', '--pileup', default=False,
+                        help='Generate samples with pileup. (Default is False)')
+    parser.add_argument('-C', '--conditions', default='phase2_realistic_T21',
+                        help='Conditions option passed to cmsDriver.py. '
+                             '(Default is phase2_realistic_T21)')
+    parser.add_argument('-R', '--era', default='Phase2C11I13M9',
+                        help='Era option passed to cmsDriver.py. (Default is Phase2C11I13M9)')
+    parser.add_argument('-m', '--memory',
+                        help='Override max memory setting in MB for CRAB. (Default is set by CRAB)')
+    parser.add_argument('-N', '--cpu', type=int,
+                        help='Override number of cores per job. (Default is 1)')
+    parser.add_argument('--no_exec', action='store_false',
+                        help='Prepare scripts but do not submit.')
+    parser.add_argument('--closeBy', action='store_true',
+                        help='Use CloseByParticleGunProducer instead of Pythia8EGun.')
+    parser.add_argument('--maxEn', type=float,
+                        help='Maximum of energy range in case of continuous energy distribution. '
+                             '(Default is 650 GeV)')
+    parser.add_argument('--minEn', type=float,
+                        help='Minimum of energy range in case of continuous energy distribution. '
+                             '(Default is 0 GeV)')
+    parser.add_argument('--maxEta', type=float,
+                        help='Maximum of eta range in case of continuous eta distribution. '
+                             '(Default is 3.0)')
+    parser.add_argument('--minEta', type=float,
+                        help='Minimum of eta range in case of continuous eta distribution. '
+                             '(Default is 1.5)')
+    parser.add_argument('--maxPhi', type=float,
+                        help='Maximum of phi range in case of continuous phi distribution. '
+                             '(Default is -pi)')
+    parser.add_argument('--minPhi', type=float,
+                        help='Minimum of phi range in case of continuous phi distribution. '
+                             '(Default is pi)')
 
     options = parser.parse_args()
 
